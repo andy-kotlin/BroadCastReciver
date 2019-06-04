cd3f778ccbcb7dc8649fcf4f5ca8119f902b9093
amin <aminuddin01697@gmail.com>
2019-06-04 17:33:28 +0530

    dynamic broadcast reciver

--- app/src/main/java/com/cootnet/june4project/MainActivity.kt
+++ app/src/main/java/com/cootnet/june4project/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.cootnet.june4project
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    var br:BroadcastReceiver? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        br = MyBroadCastReciver()
+
+        val intentFilter:IntentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+
+        registerReceiver(br, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(br)
     }
 }
--- app/src/main/java/com/cootnet/june4project/MyBroadCastReciver.kt
+++ app/src/main/java/com/cootnet/june4project/MyBroadCastReciver.kt
@@ -3,11 +3,25 @@ package com.cootnet.june4project
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class MyBroadCastReciver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
      val s:String? = intent?.action
+        when(s) {
+            Intent.ACTION_POWER_CONNECTED -> {
+                Toast.makeText(context, "connect", Toast.LENGTH_LONG).show()
+            }
+            Intent.ACTION_POWER_DISCONNECTED -> {
+                Toast.makeText(context, "disconnect", Toast.LENGTH_LONG).show()
+
+            }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                Toast.makeText(context, "plane mode changed", Toast.LENGTH_LONG).show()
+
+            }
+        }
 
     }
 }
