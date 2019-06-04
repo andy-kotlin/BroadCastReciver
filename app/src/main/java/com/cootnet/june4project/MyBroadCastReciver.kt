fa2b56b8cf516756f1a2cce358bb9fcb598d6312
amin <aminuddin01697@gmail.com>
2019-06-04 17:13:17 +0530

    Initial commit

---
+++ app/src/main/java/com/cootnet/june4project/MyBroadCastReciver.kt
@@ -0,0 +1,13 @@
+package com.cootnet.june4project
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyBroadCastReciver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+     val s:String? = intent?.action
+
+    }
+}
